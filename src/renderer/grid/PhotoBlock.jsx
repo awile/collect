49e4214e559d000dafdbdd915b32435ec3569f12
Adam Wiles <adam.wiles@newscred.com>
2020-07-12 15:26:11 -0400

    Update: Change image aspect ratio

--- src/renderer/grid/PhotoBlock.jsx
+++ src/renderer/grid/PhotoBlock.jsx
@@ -2,11 +2,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import { PlusOutlined } from '@ant-design/icons';
 import { IPCRenderer } from '../ipc';
-import { Select, Tag, Input, Tooltip, Popconfirm } from 'antd';
+import { Button, Select, Tag, Input, Tooltip, Popconfirm } from 'antd';
 const { Option } = Select;
-import { CloseOutlined } from '@ant-design/icons';
+import { PlusOutlined, CloseOutlined } from '@ant-design/icons';
 
 import './_photo-block.scss';
 
@@ -17,7 +16,8 @@ class PhotoBlock extends Component {
     this.state = {
       photoLabels: [],
       options: [],
-      searchValue: ''
+      searchValue: '',
+      addingLabel: false
     };
 
     this.handleSearch = this.handleSearch.bind(this);
@@ -54,7 +54,11 @@ class PhotoBlock extends Component {
     const responseChannel = `response-photoLabels-${moment().toISOString()}`;
     IPCRenderer.once(responseChannel, (event, photoLabelresponse) => {
       const { photoLabels } = this.state;
-      this.setState({ photoLabels: photoLabels.concat([label]), searchValue: '' });
+      this.setState({
+        addingLabel: false,
+        photoLabels: photoLabels.concat([label]),
+        searchValue: ''
+      });
     });
     IPCRenderer.send('photoLabels-request', { url: 'CREATE', body: query, responseChannel });
   }
@@ -73,40 +77,56 @@ class PhotoBlock extends Component {
 
   render() {
     const { photo, labels } = this.props;
-    const { photoLabels, searchValue } = this.state;
+    const { addingLabel, photoLabels, searchValue } = this.state;
     if (!photo) { return null; }
 
     const { location, file_type } = photo;
+    const photoLabelIds = photoLabels.map(p => p.id);
+    const photoLabelsNotAdded = labels.filter(l => !photoLabelIds.includes(l.id));
+    const dropdownOptions = searchOptions(searchValue, photoLabelsNotAdded);
 
     return (
       <div className='clt-PhotoBlock'>
         <div className='clt-PhotoBlock-container'>
           { file_type === 'mp4' ?
-          <video width='130' height='130' autoplay>
+          <video width='200' autoplay>
             <source src={`file://${location}`} type='video/mp4'></source>
           </video> :
           <img
-            width='130'
-            height='130'
+            className='clt-PhotoBlock-image'
             src={`file://${location}`} />
           }
-          <div className='clt-PhotoBlock-tags'>
-            { photoLabels.map(label =>
-              <Tag
-                key={label.id}
-                onClose={() => this.handleRemove(label)}
-                closable>{label.name}</Tag>)}
+        </div>
+        <div className='clt-PhotoBlock-tags'>
+          { photoLabels.map(label =>
+            <Tag
+              key={label.id}
+              onClose={() => this.handleRemove(label)}
+              closable>{label.name}</Tag>)
+          }
+          { addingLabel ?
             <Select
+              autoFocus
+              defaultOpen
               showSearch
+              bordered={false}
               style={{ width: 100 }}
               placeholder="add a label"
+              onBlur={() => this.setState({ addingLabel: false })}
+              filterOption={() => true}
               value={searchValue}
+              size='small'
               onSelect={this.handleSelect}
               onSearch={this.handleSearch}>
-              {searchOptions(searchValue, labels.filter(l => !photoLabels.map(p => p.id).includes(l.id))).map(l =>
-                <Option key={l.id} value={l.id}>{l.name}</Option>)}
-            </Select>
-          </div>
+              {
+                dropdownOptions.map(l => <Option key={l.id} value={l.id}>{l.name}</Option>)
+              }
+            </Select> :
+            <Button
+              size='small'
+              icon={<PlusOutlined />}
+              onClick={() => this.setState({ addingLabel: true })} />
+          }
         </div>
       </div>
     );
