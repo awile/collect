1ffaa6ee78aa4689c6e1dbe37788a04e0abdefad
Adam Wiles <adam.wiles@newscred.com>
2020-07-18 22:12:51 -0400

    Feature: Move db to documents for backup on icloud

--- src/main/db/index.js
+++ src/main/db/index.js
@@ -3,11 +3,12 @@ import * as knex from 'knex';
 import { setupDBTables } from './schemas/';
 import config from './config';
 import { newUuid } from './utils';
+import { app } from 'electron';
 
 const conn = knex({
   client: 'sqlite3',
   connection: {
-    filename: config.db.filename
+    filename: `${app.getPath('documents')}/collect/${config.db.filename}`
   }
 });
 
