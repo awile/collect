5627c9b1fbe0f61a800bbda3468501a0a57a73a7
Adam Wiles <adam.wiles@newscred.com>
2020-08-01 12:33:05 -0400

    Refactor: Rewrite navbar as function component

--- src/renderer/nav-bar/index.jsx
+++ src/renderer/nav-bar/index.jsx
@@ -1,93 +1,70 @@
-import React, { Component } from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { AutoComplete, Input, Layout } from 'antd';
 
-const { Header, Content, Sider } = Layout;
+const { Header } = Layout;
 const { Search } = Input;
 
 import './_nav-bar.scss';
 
-class NavBar extends Component {
-  constructor() {
-    super();
+function NavBar({ labels, selectedLabelName, onChange }) {
+  const [options, setOptions] = useState([]);
+  const [value, setValue] = useState('');
+  const searchInputRef = useRef();
 
-    this.state = {
-      options: [],
-      value: ''
-    };
-
-    this.handleSearch = this.handleSearch.bind(this);
-    this.handleSelect = this.handleSelect.bind(this);
-    this.ref = React.createRef();
+  const makeOptions = (labels) => labels.map(l => ({ label: l.name, value: l.name, id: l.id }));
+  if (!options.every(o => labels.findIndex(l => l.id === o.id) >= 0)) {
+    const newOptions = makeOptions(labels);
+    setOptions(newOptions);
   }
 
-  componentDidUpdate(prevProps) {
-    const { labels } = this.props;
-    if (prevProps.labels.length !== labels.length) {
-
-      this.setState({ options: labels.map(l => ({ label: l.name, value: l.name, id: l.id })) });
-    }
+  const handleSearch = (value) => {
+    const filterOptions = makeOptions(labels
+      .filter(o => o.name.toLowerCase().indexOf(value.toLowerCase()) >= 0))
+    setValue(value);
+    setOptions(filterOptions);
   }
 
-  handleSearch(value) {
-    const { labels } = this.props;
-    const { options } = this.state;
-    const filterOptions = labels
-      .filter(o => o.name.toLowerCase().indexOf(value.toLowerCase()) >= 0)
-      .map(l => ({ label: l.name, value: l.name, id: l.id }));
-    this.setState({ options: filterOptions, value });
-  }
-
-  handleSelect(label, option) {
-    const { labels, onChange } = this.props;
+  const handleSelect = (_, option) => {
     if (onChange) {
       const label = labels.find(l => l.id === option.id);
       onChange(label);
     }
-    this.setState({
-      options: labels.map(l => ({ label: l.name, value: l.name, id: l.id })),
-      value: ''
-    });
-    if (this.ref) {
-      this.ref.current.blur();
-    }
-  }
+    setValue('');
+    setOptions(makeOptions(labels));
+    searchInputRef.current.blur();
+  };
 
-  render() {
-    const { onChange, selectedLabelName } = this.props;
-    const { options, value } = this.state;
-
-    return (
-      <Header className='clt-NavBar'>
-        <div className='clt-NavBar-container'>
-          <span className='clt-NavBar-title'>1-800-COLLECT</span>
-          { selectedLabelName && (
-            <React.Fragment>
-              <span className='clt-NavBar-divider'>></span>
-              <span className='clt-NavBar-current'>{selectedLabelName}</span>
-            </React.Fragment>
-          )}
-        </div>
-        <AutoComplete
-          className='clt-NavBar-search-bar'
-          dropdownMatchSelectWidth={252}
-          style={{ width: 300 }}
-          options={options}
+  return (
+    <Header className='clt-NavBar'>
+      <div className='clt-NavBar-container'>
+        <span className='clt-NavBar-title'>1-800-COLLECT</span>
+        { selectedLabelName && (
+          <React.Fragment>
+            <span className='clt-NavBar-divider'>{'>'}</span>
+            <span className='clt-NavBar-current'>{selectedLabelName}</span>
+          </React.Fragment>
+        )}
+      </div>
+      <AutoComplete
+        className='clt-NavBar-search-bar'
+        dropdownMatchSelectWidth={252}
+        style={{ width: 300 }}
+        options={options}
+        value={value}
+        onSelect={handleSelect}
+        onChange={handleSearch}>
+        <Search
+          ref={searchInputRef}
+          allowClear
+          className='clt-NavBar-search-input'
+          placeholder='search'
+          style={{ width: 325 }}
           value={value}
-          onSelect={this.handleSelect}
-          onChange={this.handleSearch}>
-          <Search
-            ref={this.ref}
-            allowClear
-            className='clt-NavBar-search-input'
-            placeholder='search'
-            style={{ width: 325 }}
-            value={value}
-            size='small' />
-        </AutoComplete>
-      </Header>
-    );
-  }
+          size='small' />
+      </AutoComplete>
+    </Header>
+  );
 }
 
 NavBar.propTypes = {
